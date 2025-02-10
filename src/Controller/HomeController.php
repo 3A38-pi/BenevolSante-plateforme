b629c1a1f3846bbd5cc80acc4cb31b50106baf00
Yasser Wahada <wahadayasser02@gmail.com>
2025-02-10 02:52:30 +0100

    Integration : Template Front Office

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+
+
+
+    
+    #[Route('/home', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('home/Home.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/services', name: 'services')]
+    public function index2(): Response
+    {
+        return $this->render('services/services.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/doctors', name: 'doctors')]
+    public function index3(): Response
+    {
+        return $this->render('doctors/doctors.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/appointment', name: 'appointment')]
+    public function index4(): Response
+    {
+        return $this->render('appointment/appointment.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    #[Route('/testimonial', name: 'testimonial')]
+    public function index5(): Response
+    {
+        return $this->render('testimonial/testimonial.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function index6(): Response
+    {
+        return $this->render('contact/contact.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+
+}
