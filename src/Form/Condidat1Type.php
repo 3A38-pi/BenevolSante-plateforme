18cf536eb9e8d2d7977e3b6611d3b2dfa540a260
AYOUUUUUUUB <ayoyb.amiri@esprit.tn>
2025-02-17 23:19:12 +0100

    controle de saisie back offre et condidat

--- src/Form/Condidat1Type.php
+++ src/Form/Condidat1Type.php
@@ -8,20 +8,42 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 class Condidat1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
-            ->add('telephone')
-            ->add('cv')
+        ->add('nom', TextType::class, [
+            'label' => 'Nom',
+            'required' => true, // Or false if optional
+            'empty_data' => '', // Ensure default value is empty string if not filled
+        ])
+        ->add('prenom', TextType::class, [
+            'label' => 'Prénom',
+            'required' => true, // Or false if optional
+            'empty_data' => '', // Default empty string if not filled
+        ])
+        ->add('email', EmailType::class, [
+            'label' => 'Email',
+            'required' => true, // Should be required for email field
+            'empty_data' => '', // Default empty string if not filled
+        ])
+        ->add('telephone', TelType::class, [
+            'label' => 'Téléphone',
+            'required' => true, // Or false if optional
+            'empty_data' => '', // Default empty string if not filled
+        ])
+            ->add('cv', TextType::class, [
+                'required' => false, 
+                'empty_data' => '', // Set default empty string if no value is provided
+            ])
             ->add('offre', EntityType::class, [
                 'class' => Offre::class,
-'choice_label' => 'id',
+'choice_label' => 'titre_offre',
             ])
         ;
     }
--- src/Form/Offre1Type.php
+++ src/Form/Offre1Type.php
@@ -6,14 +6,22 @@ use App\Entity\Offre;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class Offre1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre_offre')
-            ->add('description_offre')
+        ->add('titre_offre', TextType::class, [
+            'required' => true,
+            'empty_data' => '',  // Valeur par défaut si le champ est vide
+        ])
+        ->add('description_offre', TextareaType::class, [
+            'required' => false,
+            'empty_data' => '',
+        ])
         ;
     }
 
