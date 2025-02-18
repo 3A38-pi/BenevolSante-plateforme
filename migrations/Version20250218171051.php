4d28056808fb907149ff91e84ef745bea1d5cb80
DeganiOmaar <deganiomar2001@gmail.com>
2025-02-18 18:22:48 +0100

    gestion Article : date de creation du commentaire

--- migrations/Version20250218171051.php
+++ migrations/Version20250218171051.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250218095914 extends AbstractMigration
+final class Version20250218171051 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250218095914 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article CHANGE tags categorie VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE commentaire ADD created_at DATETIME DEFAULT NULL, ADD etat VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE article CHANGE categorie tags VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE commentaire DROP created_at, DROP etat');
     }
 }
