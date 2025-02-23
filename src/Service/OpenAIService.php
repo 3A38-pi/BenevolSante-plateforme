993f7e3e93816481e91b3829123987222a6bcc9d
DeganiOmaar <deganiomar2001@gmail.com>
2025-02-23 23:23:57 +0100

    restruction des commentaires interdits

--- src/Service/OpenAIService.php
+++ src/Service/OpenAIService.php
@@ -19,36 +19,56 @@ class OpenAIService
      * Vérifie si le commentaire est acceptable.
      * Ici, on vérifie simplement la présence de mots interdits.
      */
-    public function isCommentAcceptable(string $comment): bool
-    {
-        // Liste des mots interdits (à adapter selon vos besoins)
-        $forbiddenWords = [
-            // Profanités en anglais
-            'fuck', 'fucking', 'motherfucker', 'shit', 'bitch', 'asshole', 'dick', 'cunt', 'bastard', 'damn',
-            'ass', 'asshole', 'assholes', 'asshole',
-            'bitch', 'bitches', 'bitch',
+    // public function isCommentAcceptable(string $comment): bool
+    // {
+    //     // Liste des mots interdits (à adapter selon vos besoins)
+    //     $forbiddenWords = [
+    //         // Profanités en anglais
+    //         'fuck', 'fucking', 'motherfucker', 'shit', 'bitch', 'asshole', 'dick', 'cunt', 'bastard', 'damn',
+    //         'ass', 'asshole', 'assholes', 'asshole',
+    //         'bitch', 'bitches', 'bitch',
             
     
-            // Profanités en français
-            'putain', 'merde', 'connard', 'connasse', 'enfoiré', 'enfoirée', 'foutre', 'nique', 'nique ta mère', 'salope',
-            'enculé', 'enculer', 'fils de pute', 'ta gueule', 'cul', 'bite', 'couille', 'chier', 'bordel', 'abruti', 'abrutie',
-            'pd', 'trou du cul', 'chiant', 'chiante', 'va te faire foutre', 
-            'fou', 'fouille', 'fouillette', 'fouilleuse', 'fouilleuses', 'fouilleuse', 'fouilleuses',
+    //         // Profanités en français
+    //         'putain', 'merde', 'connard', 'connasse', 'enfoiré', 'enfoirée', 'foutre', 'nique', 'nique ta mère', 'salope',
+    //         'enculé', 'enculer', 'fils de pute', 'ta gueule', 'cul', 'bite', 'couille', 'chier', 'bordel', 'abruti', 'abrutie',
+    //         'pd', 'trou du cul', 'chiant', 'chiante', 'va te faire foutre', 
+    //         'fou', 'fouille', 'fouillette', 'fouilleuse', 'fouilleuses', 'fouilleuse', 'fouilleuses',
 
-        ];
-        $commentLower = strtolower($comment);
+    //     ];
+    //     $commentLower = strtolower($comment);
 
-        // Parcourir la liste des mots interdits
-        foreach ($forbiddenWords as $word) {
-            if (strpos($commentLower, strtolower($word)) !== false) {
-                // Si un mot interdit est trouvé, le commentaire n'est pas acceptable
-                return false;
-            }
+    //     // Parcourir la liste des mots interdits
+    //     foreach ($forbiddenWords as $word) {
+    //         if (strpos($commentLower, strtolower($word)) !== false) {
+    //             // Si un mot interdit est trouvé, le commentaire n'est pas acceptable
+    //             return false;
+    //         }
+    //     }
+
+    //     // Si aucun mot interdit n'est trouvé, le commentaire est acceptable
+    //     return true;
+    // }
+    public function isCommentAcceptable(string $comment): bool
+{
+    // Chemin absolu ou relatif vers le fichier ignoré
+    $filePath = __DIR__ . '/../forbidden_words.json';
+    $jsonContent = file_get_contents($filePath);
+    $forbiddenWords = json_decode($jsonContent, true);
+
+    // Convertir le commentaire en minuscules
+    $commentLower = strtolower($comment);
+
+    // Vérifier la présence de mots interdits
+    foreach ($forbiddenWords as $word) {
+        if (strpos($commentLower, strtolower($word)) !== false) {
+            return false;
         }
-
-        // Si aucun mot interdit n'est trouvé, le commentaire est acceptable
-        return true;
     }
+
+    return true;
+}
+
 
     // Vous pouvez ajouter ici d'autres méthodes, par exemple pour des appels à l'API OpenAI
 }
